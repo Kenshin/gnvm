210191bc9c702af0439940bf42687e6a33e4c40e
kenshin <fxblog@gmail.com>
2014-05-04 12:15:20 +0800

    gnmv cli technology selection

--- gnvm.go
+++ gnvm.go
@@ -4,6 +4,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/spf13/cobra"
 )
 
 func flagMode() {
@@ -70,6 +71,58 @@ func flagMode() {
 	fmt.Printf("NFlag = %d\n", flag.NFlag())
 }
 
+func cobraMode() {
+
+	var (
+		Global bool
+	)
+
+	// defind root cmd
+	var GnvmCmd = &cobra.Command{
+		Use:   "gnvm",
+		Short: "Hugo is a very fast static site generator",
+		Long: `A Fast and Flexible Static Site Generator built with
+	            love by spf13 and friends in Go.
+	            Complete documentation is available at http://hugo.spf13.com`,
+		Run: func(cmd *cobra.Command, args []string) {
+			// Do Stuff Here
+		},
+	}
+
+	// sub cmd
+	var versionCmd = &cobra.Command{
+		Use:   "version",
+		Short: "Print the version number of Hugo",
+		Long:  `All software has versions. This is Hugo's`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println(args)
+			fmt.Println("Hugo Static Site Generator v0.9 -- HEAD")
+		},
+	}
+
+	var installCmd = &cobra.Command{
+		Use:   "install",
+		Short: " install latest node.js version",
+		Long:  `All software has versions. This is Hugo's`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println(Global)
+			fmt.Println(args)
+			fmt.Println(" install latest node.js version")
+		},
+	}
+
+	// add sub cmd to root
+	GnvmCmd.AddCommand(versionCmd)
+	GnvmCmd.AddCommand(installCmd)
+
+	// set flag to root
+	GnvmCmd.PersistentFlags().BoolVarP(&Global, "global", "g", false, "golbal output")
+
+	// exec
+	GnvmCmd.Execute()
+}
+
 func main() {
-	flagMode()
+	//flagMode()
+	cobraMode()
 }
