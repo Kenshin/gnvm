5abd490fdb55e2a0f03ef1e58971591867ecd582
kenshin <fxblog@gmail.com>
2014-05-05 13:52:45 +0800

    add gnvmrc frame work

--- config/config.go
+++ config/config.go
@@ -12,30 +12,54 @@ import (
 const (
 	VERSION  = "0.1.0"
 	CONFIG   = ".gnvmrc"
-	REGISTRY = "http://nodejs.org/dist/"
-	NODEROOT = ""
+	REGISTRY = `"http://nodejs.org/dist/"`
+	NODEROOT = `""`
 )
 
 func init() {
 	config, err := toml.LoadFile(CONFIG)
 	if err != nil {
+
+		// print error
 		fmt.Println("Error ", err.Error())
+
+		// create .gnvmrc file and write
 		createConfigFile()
 	} else {
+
+		// get registry
 		registry := config.Get("registry").(string)
-		fmt.Println(" registry is " + registry)
+		fmt.Println("registry is " + registry)
+
+		// get nodeversion
+		registry := config.Get("noderoot").(string)
+		fmt.Println("noderoot is " + noderoot)
 	}
 }
 
 func createConfigFile() {
+
+	// create file
 	file, err := os.Create(CONFIG)
 	if err != nil {
-		fmt.Println(".gnvmrc create fail.")
-	} else {
-		file.WriteString("registry=" + REGISTRY)
-		file.WriteString("\n")
-		file.WriteString("noderoot=" + NODEROOT)
-		fmt.Println(".gnvmrc file create success.")
+		fmt.Println(".gnvmrc create fail, error is" + err.Error())
+		return
 	}
+
+	//write file
+	_, fileErr := file.WriteString("registry=" + REGISTRY + "\n" + "noderoot=" + NODEROOT)
+	if fileErr != nil {
+		fmt.Println("write .gnvmrc fail" + fileErr.Error())
+		return
+	}
+
+	// close file
+	file.Close()
+
+	// defear
+	defer file.Close()
+
+	// success
+	fmt.Println(".gnvmrc file create success.")
 
 }
