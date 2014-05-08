2cf264d500b171d417e7ae9adab19f5e7689b60d
kenshin <fxblog@gmail.com>
2014-05-08 17:23:50 +0800

    add gnvm ls test code

--- nodehandle/nodehandle.go
+++ nodehandle/nodehandle.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strconv"
 	"strings"
 
@@ -249,5 +250,17 @@ func Uninstall(folder string) {
 }
 
 func LS() {
-	fmt.Println("LS")
+	err := filepath.Walk(rootPath, func(path string, f os.FileInfo, err error) error {
+		if f == nil {
+			return err
+		}
+		if f.IsDir() {
+			return nil
+		}
+		println(path)
+		return nil
+	})
+	if err != nil {
+		fmt.Printf("filepath.Walk() returned %v\n", err)
+	}
 }
