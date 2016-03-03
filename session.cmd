980eb0ab5022697e16a90427fbdc1c103f621918
Kenshin <fxblog@gmail.com>
2016-03-03 12:06:22 +0800

    Remove 'session reg' logic.

--- session.cmd
+++ session.cmd
@@ -7,10 +7,6 @@ if not defined NODE_HOME (
     set "NODE_HOME=%~dp0"
 )
 
-:: Add %GNVM_SESSION_HOME% to path
-set GNVM_SESSION_HOME="%NODE_HOME%"
-set path=%GNVM_SESSION_HOME%;%path%
-
 ::===========================================================
 :: Logic
 ::===========================================================
@@ -19,7 +15,6 @@ if "%1" == "help"    goto help
 if "%1" == "run"     goto run
 if "%1" == "exit"    goto exit
 if "%1" == "version" goto version
-if "%1" == "reg"     goto reg
 
 ::===========================================================
 :: help : Show help message
@@ -33,14 +28,12 @@ echo   session [command]
 echo;
 echo Commands:
 echo   run                   Set session node.exe version.
-echo   reg                   Set env GNVM_SESSION_HOME to regedit.
 echo   exit                  Quit session node.exe version.
 echo   version               Show version.
 echo;
 echo Example:
 echo   session help          Show session cli command help.
 echo   session run 0.10.24   Set 0.10.24 is session node.exe verison.
-echo   session reg           Set GNVM_SESSION_HOME to environment variable.
 echo   session exit          Quit sesion node.exe, restore global node.exe version.
 echo   session version       Show version.
 goto quit
@@ -90,30 +83,6 @@ set path=%NODE_HOME%;%path%
 
 @echo on
 @echo Exit session successful.
-@echo off
-goto quit
-
-::===========================================================
-:: regedit : Add GNVM_SESSION_HOME to regedit
-::===========================================================
-:reg
-
-@echo off
-:: runas /user:<admin use> xxx, e.g. runas /user:Kenshin cmd
-:: HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment
-:: HKEY_CURRENT_USER\Environment
-
-:: Get origin path
-for /f "tokens=1,2,3,4,*" %%i in ('reg query "HKEY_CURRENT_USER\Environment" ^| find /i "path"') do SET "OldPath=%%k"
-
-:: Add GNVM_SESSION_HOME to regedit
-set RegPath=HKEY_CURRENT_USER\Environment
-reg add "%RegPath%" /v GNVM_SESSION_HOME /t REG_SZ /d "%NODE_HOME%"
-reg add "%RegPath%" /v path /t REG_SZ /d "%NODE_HOME%";%OldPath%
-
-@echo on
-@echo Regedit GNVM_SESSION_HOME=%GNVM_SESSION_HOME%.
-@echo Regedit successful.
 @echo off
 goto quit
 
