b3e19f7540616632e0a2f6af04bbd114eae50409
Kenshin <fxblog@gmail.com>
2016-03-03 18:18:18 +0800

    Change 'session exit' to 'session clear'.

--- session.cmd
+++ session.cmd
@@ -13,7 +13,7 @@ if not defined NODE_HOME (
 if "%1" == ""        goto help
 if "%1" == "help"    goto help
 if "%1" == "run"     goto run
-if "%1" == "exit"    goto exit
+if "%1" == "clear"    goto clear
 if "%1" == "version" goto version
 
 ::===========================================================
@@ -28,13 +28,13 @@ echo   session [command]
 echo;
 echo Commands:
 echo   run                   Set session node.exe version.
-echo   exit                  Quit session node.exe version.
+echo   clear                 Quit session node.exe version.
 echo   version               Show version.
 echo;
 echo Example:
 echo   session help          Show session cli command help.
 echo   session run 0.10.24   Set 0.10.24 is session node.exe verison.
-echo   session exit          Quit sesion node.exe, restore global node.exe version.
+echo   session clear         Quit sesion node.exe, restore global node.exe version.
 echo   session version       Show version.
 goto quit
 
@@ -71,10 +71,10 @@ set path=%GNVM_SESSION_NODE_HOME%;%path%
 @echo Startup session node.exe version %2.
 @echo Important:
 @echo - if node.exe work on session version, "gnvm install -g", "gnvm update -g" "gnvm use x.xx.xx" can't be use.
-@echo - if quit/remove session, you must use "session exit".
+@echo - if quit/remove session, you must use "session clear".
 @echo - if on "%NODE_HOME%" directory, unable to "run %2".
 @echo - if on "%NODE_HOME%" directory, auto goto "%NODE_HOME%\gnvm_session" directory.
-@echo - if on "%NODE_HOME%\gnvm_session" directory, use "session exit" auto previous directory.
+@echo - if on "%NODE_HOME%\gnvm_session" directory, use "session clear" auto previous directory.
 @echo off
 goto quit
 
@@ -100,9 +100,9 @@ cd %GNVM_SESSION_HOME%
 goto quit
 
 ::===========================================================
-:: exit : Quit/Remove session node.exe version
+:: clear : Quit/Remove session node.exe version
 ::===========================================================
-:exit
+:clear
 
 if defined ORI_GNVM_SESSION_PATH (
     cd %ORI_GNVM_SESSION_PATH%
