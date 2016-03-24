2c1419ab4c8a8f100ae355a9f6229cce0d8d438e
Kenshin <fxblog@gmail.com>
2016-03-24 15:06:40 +0800

    Adapter regedit lib.

--- nodehandle/reg.go
+++ nodehandle/reg.go
@@ -73,13 +73,13 @@ func Reg(s string) {
 }
 
 func regAdd(key, value string) ([]util.Reg, error) {
-	reg := util.Regedit{util.Actions[util.Add], util.Fields[util.HKCU] + "\\Environment", key, util.Types[util.SZ], value}
-	regcmd := reg.Add()
+	reg := util.Regedit{Action: util.Actions[util.Add], Field: util.Fields[util.HKCU] + "\\Environment"}
+	regcmd := reg.Add(util.Reg{key, util.Types[util.SZ], value})
 	return regcmd.Exec()
 }
 
 func regQuery(key string) ([]util.Reg, error) {
-	reg := util.Regedit{Action: util.Actions[util.Query], Field: util.Fields[util.HKCU] + "\\Environment", Key: key}
-	regcmd := reg.Search()
+	reg := util.Regedit{Action: util.Actions[util.Query], Field: util.Fields[util.HKCU] + "\\Environment"}
+	regcmd := reg.Search(util.Reg{Key: key})
 	return regcmd.Exec()
 }
