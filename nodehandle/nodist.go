f1fa251e2ec0d0d5c5ff0fbcb4f7c5cdf51d3a95
Kenshin <fxblog@gmail.com>
2016-03-28 14:16:31 +0800

    Remove old source.

--- nodehandle/nodist.go
+++ nodehandle/nodist.go
@@ -106,64 +106,6 @@ func New(url string, filter *regexp.Regexp) (*Nodeist, error, int) {
 }
 
 /*
-func (this *NL) Len() int {
-	return len(*this)
-}
-
-func (this *NL) Less(i, j int) bool {
-	v1, v2 := (*this)[i], (*this)[j]
-	float1, float2 := util.FormatNodeVer(v1.Node.Version), util.FormatNodeVer(v2.Node.Version)
-	switch {
-	case float1 > float2:
-		return false
-	case float1 < float2:
-		return true
-	}
-}
-*/
-
-/*
-func (nl NL) New(idx int, value map[string]interface{}) NodeList {
-	ver, _ := value["version"].(string)
-	date, _ := value["date"].(string)
-	npm, _ := value["npm"].(string)
-	if npm == "" {
-		npm = "[x]"
-	}
-	exe := filter(ver[1:])
-	nl[ver] = NodeList{idx, date, Node{ver, exe}, NPM{npm}}
-	return nl[ver]
-}
-
-
-func (this NL) Filter(idx int, value map[string]interface{}, regx *regexp.Regexp) (NodeList, bool) {
-	isfilter := false
-	ver, _ := value["version"].(string)
-	if ok := regx.MatchString(ver[1:]); ok {
-		date, _ := value["date"].(string)
-		npm, _ := value["npm"].(string)
-		if npm == "" {
-			npm = "[x]"
-		}
-		exe := filter(ver[1:])
-		this[ver] = NodeList{idx, date, Node{ver, exe}, NPM{npm}}
-		isfilter = true
-	}
-	return this[ver], isfilter
-}
-
-func (nl NL) IndexBy(key string) {
-	sorts = append(sorts, key)
-}
-
-
-func (nl *NL) Print(nodeist NodeList) {
-	msg := fmt.Sprintf("id: %v date: %v node version: %v os support: %v npm version: %v", nodeist.ID, nodeist.Date, nodeist.Node.Version, nodeist.Node.Exec, nodeist.NPM.Version)
-	fmt.Println(msg)
-}
-*/
-
-/*
  Print NodeDetail collection
 
  Param:
