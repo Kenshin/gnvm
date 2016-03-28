408dc3e4ff6ba57af331343451c51783513133e5
Kenshin <fxblog@gmail.com>
2016-03-28 17:01:57 +0800

    Add 'gnvm npm x.xx.xx' and optimize 'gnvm install and gnvm uninstall' npm keyworkd flow.

--- nodehandle/nodehandle.go
+++ nodehandle/nodehandle.go
@@ -5,7 +5,6 @@ import (
 	// lib
 	. "github.com/Kenshin/cprint"
 	"github.com/Kenshin/curl"
-	"github.com/pierrre/archivefile/zip"
 
 	// go
 	//"log"
@@ -17,7 +16,7 @@ import (
 	"runtime"
 	"strconv"
 	"strings"
-	"time"
+	//"time"
 
 	// local
 	"gnvm/config"
@@ -231,40 +230,6 @@ func Uninstall(folder string) {
 	P(DEFAULT, "Node.exe version %v uninstall success.\n", folder)
 }
 
-func UninstallNpm() {
-
-	// try catch
-	defer func() {
-		if err := recover(); err != nil {
-			Error(ERROR, "'gnvm uninstall npm' an error has occurred. please check. \nError: ", err)
-			os.Exit(0)
-		}
-	}()
-
-	removeFlag := true
-
-	if !isDirExist(rootPath+"npm.cmd") && !isDirExist(rootPath+"node_modules"+util.DIVIDE+"npm") {
-		P(WARING, "%v not exist %v.\n", rootPath, "npm.cmd")
-		return
-	}
-
-	// remove npm.cmd
-	if err := os.RemoveAll(rootPath + "npm.cmd"); err != nil {
-		removeFlag = false
-		P(ERROR, "remove %v file fail from %v, Error: %v.\n", "npm.cmd", rootPath, err.Error())
-	}
-
-	// remove node_modules/npm
-	if err := os.RemoveAll(rootPath + "node_modules" + util.DIVIDE + "npm"); err != nil {
-		removeFlag = false
-		P(ERROR, "remove %v folder fail from %v, Error: %v.\n", "npm", rootPath+"node_modules", err.Error())
-	}
-
-	if removeFlag {
-		P(DEFAULT, "npm uninstall success from %v.\n", rootPath)
-	}
-}
-
 func LS(isPrint bool) ([]string, error) {
 
 	// try catch
@@ -409,20 +374,14 @@ func Install(args []string, global bool) int {
 				P(ERROR, "%v format error, parameter must be '%v' or '%v'.\n", v, "x.xx.xx", "x.xx.xx-x86|x64")
 			case "4":
 				P(ERROR, "%v format error, the correct format is %v or %v. \n", v, "0.xx.xx", "^0.xx.xx")
+			case "5":
+				P(WARING, "'%v' not logger support command, please usage '%v'. See '%v'.\n", "gnvm install npm", "gnvm npm x.xx.xx", "gnvm help npm")
 			}
 			continue
 		}
 
-		v = util.EqualAbs("latest", v)
-		v = util.EqualAbs("npm", v)
-
-		// check npm
-		if ver == "npm" {
-			P(WARING, "use format error, the correct format is '%v'. See '%v'.\n", "gnvm install npm", "gnvm help install")
-			continue
-		}
-
 		// check latest and get remote latest
+		v = util.EqualAbs("latest", v)
 		if ver == config.LATEST {
 			localVersion = config.GetConfig(config.LATEST_VERSION)
 			P(NOTICE, "local  latest version is %v.\n", localVersion)
@@ -492,100 +451,6 @@ func Install(args []string, global bool) int {
 	}
 
 	return code
-
-}
-
-func InstallNpm() {
-
-	// try catch
-	defer func() {
-		if err := recover(); err != nil {
-			if strings.HasPrefix(err.(string), "CURL Error:") {
-				fmt.Printf("\n")
-			}
-			Error(ERROR, "'gnvm install npm' an error has occurred. \nError: ", err)
-			os.Exit(0)
-		}
-	}()
-
-	out, err := exec.Command(rootPath+"npm", "--version").Output()
-	if err == nil {
-		P(WARING, "current path %v exist npm, version is %v", rootPath, string(out[:]), "\n")
-		return
-	}
-
-	url := config.GetConfig(config.REGISTRY) + "npm"
-
-	// get
-	code, res, _ := curl.Get(url)
-	if code != 0 {
-		return
-	}
-	// close
-	defer res.Body.Close()
-
-	maxTime, _ := time.Parse(TIMEFORMART, TIMEFORMART)
-	var maxVersion string
-
-	getNpmVersion := func(content string, line int) bool {
-		if strings.Index(content, `<a href="`) == 0 && strings.Contains(content, ".zip") {
-
-			// parse
-			newLine := strings.Replace(content, `<a href="`, "", -1)
-			newLine = strings.Replace(newLine, `</a`, "", -1)
-			newLine = strings.Replace(newLine, `">`, " ", -1)
-
-			// e.g. npm-1.3.9.zip npm-1.3.9.zip> 23-Aug-2013 21:14 1535885
-			orgArr := strings.Fields(newLine)
-
-			// e.g. npm-1.3.9.zip
-			version := orgArr[0:1][0]
-
-			// e.g. 23-Aug-2013 21:14
-			sTime := strings.Join(orgArr[2:len(orgArr)-1], " ")
-
-			// bubble sort
-			if t, err := time.Parse(TIMEFORMART, sTime); err == nil {
-				if t.Sub(maxTime).Seconds() > 0 {
-					maxTime = t
-					maxVersion = version
-				}
-			}
-		}
-		return false
-	}
-
-	if err := curl.ReadLine(res.Body, getNpmVersion); err != nil && err != io.EOF {
-		P(ERROR, "parse npm version Error: %v, from %v\n", err, url)
-		return
-	}
-
-	if maxVersion == "" {
-		P(ERROR, "get npm version fail from %v, please check. See '%v'.\n", url, "gnvm help config")
-		return
-	}
-
-	P(NOTICE, "the latest version is %v from %v.\n", maxVersion, config.GetConfig(config.REGISTRY))
-
-	// download zip
-	zipPath := os.TempDir() + util.DIVIDE + maxVersion
-	if code := downloadNpm(maxVersion); code == 0 {
-
-		P(DEFAULT, "Start unarchive file %v.\n", maxVersion)
-
-		//unzip(maxVersion)
-
-		if err := zip.UnarchiveFile(zipPath, config.GetConfig(config.NODEROOT), nil); err != nil {
-			panic(err)
-		}
-
-		P(DEFAULT, "End unarchive.\n")
-	}
-
-	// remove temp zip file
-	if err := os.RemoveAll(zipPath); err != nil {
-		P(ERROR, "remove zip file fail from %v, Error: %v.\n", zipPath, err.Error())
-	}
 
 }
 
@@ -818,23 +683,4 @@ func copy(src, dest string) error {
 	_, err := io.Copy(dstFile, srcFile)
 
 	return err
-}
-
-/*
- * return code
- * 0: success
- *
- */
-func downloadNpm(version string) int {
-
-	/*
-		// set url
-		url := config.GetConfig(config.REGISTRY) + "npm/" + version
-		// download
-		if code := curl.New(url, version, os.TempDir()+DIVIDE+version); code != 0 {
-			return code
-		}
-	*/
-
-	return 0
 }
