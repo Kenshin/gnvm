af1279ef9e872250f0e808f37684aad1cb234427
Kenshin <fxblog@gmail.com>
2016-04-03 16:53:27 +0800

    Optimize 'gnvm install' copywrite.

--- nodehandle/nodehandle.go
+++ nodehandle/nodehandle.go
@@ -164,7 +164,7 @@ func InstallNode(args []string, global bool) int {
 
 		// when os is 386, not download 64 bit node.exe
 		if runtime.GOARCH == "386" && suffix == "x64" {
-			P(WARING, "current operating system is %v, not support %v suffix.\n", "32-bit", "-x64")
+			P(WARING, "current operating system is %v, not support %v suffix.\n", "32-bit", "x64")
 			continue
 		}
 
